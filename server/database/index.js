955394492ca33d8fe73a5d1a1df0cf722eebc635
Ian Wilson <iwilsonq@gmail.com>
2017-10-15 18:24:35 -0700

    log

--- server/database/index.js
+++ server/database/index.js
@@ -6,6 +6,12 @@ const database = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
+console.log({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+})
 
 const getSql = query => {
   return new Promise((resolve, reject) => {
